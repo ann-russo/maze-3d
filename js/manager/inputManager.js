23dceb9a146d24aa9b6f29f95ba6d51d4a6f2ff5
Anastasia Russo <russo.anastasia@proton.me>
2023-11-15 23:10:31 +0100

    Code clean-up and refactoring in inputManager

--- js/manager/inputManager.js
+++ js/manager/inputManager.js
@@ -1,40 +1,57 @@
-var InputManager =
-{
-    keys: [],
-    oldKeys: [],
+/**
+ * InputManager handles keyboard inputs for the game.
+ */
+var InputManager = {
+    keys: {},
+    oldKeys: {},
 
-    update: function()
-    {
-        this.oldKeys = this.keys.clone();
+    /**
+     * Updates the state of keys.
+     */
+    update: function() {
+        this.oldKeys = { ...this.keys };
     },
 
-    keyDown: function( code )
-    {
-        this.keys[ code ] = true;
+    /**
+     * Handles key down events.
+     * @param {number} code - The key code of the pressed key.
+     */
+    keyDown: function(code) {
+        this.keys[code] = true;
     },
 
-    keyUp: function( code )
-    {
-        this.keys[ code ] = false;
+    /**
+     * Handles key up events.
+     * @param {number} code - The key code of the released key.
+     */
+    keyUp: function(code) {
+        this.keys[code] = false;
     },
 
-    isKeyDown: function( code )
-    {
-        return ( this.keys[ code ] === true );
+    /**
+     * Checks if a key is currently pressed down.
+     * @param {number} code - The key code to check.
+     * @returns {boolean} True if the key is down, false otherwise.
+     */
+    isKeyDown: function(code) {
+        return !!this.keys[code];
     },
 
-    isKeyPressed: function( code )
-    {
-        return ( this.keys[ code ] === true && this.oldKeys[ code ] !== true );
-    },
+    /**
+     * Checks if a key was just pressed.
+     * @param {number} code - The key code to check.
+     * @returns {boolean} True if the key was just pressed, false otherwise.
+     */
+    isKeyPressed: function(code) {
+        return !!this.keys[code] && !this.oldKeys[code];
+    }
 };
 
-document.onkeydown = function( evnt )
-{
-    InputManager.keyDown(evnt.keyCode);
-};
+// Event listeners for key down and key up events
+document.addEventListener('keydown', function(event) {
+    InputManager.keyDown(event.keyCode);
+});
 
-document.onkeyup = function( evnt )
-{
-    InputManager.keyUp(evnt.keyCode);
-};
+document.addEventListener('keyup', function(event) {
+    InputManager.keyUp(event.keyCode);
+});
